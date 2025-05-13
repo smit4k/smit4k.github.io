f7d6700780032e7774abbfc2593082d59a42a4b9
Saber <smit.patil.usa@gmail.com>
2025-05-13 18:38:26 -0400

    Remove unnecessary comments from app.js

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
-/* -----------------------------------------------
-/* How to use? : Check the GitHub README
-/* ----------------------------------------------- */
-
-
 particlesJS.load('particles-js', 'assets/particles.json', function() {
   console.log('particles.js loaded - callback');
 });
