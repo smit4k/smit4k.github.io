1731db2289a0f10fd28c28d1366bdbd4d2bced9e
Saber <smit.patil.usa@gmail.com>
2025-05-13 18:25:36 -0400

    Replace old background with particles background

---
+++ app.js
@@ -0,0 +1,8 @@
+/* -----------------------------------------------
+/* How to use? : Check the GitHub README
+/* ----------------------------------------------- */
+
+
+particlesJS.load('particles-js', 'assets/particles.json', function() {
+  console.log('particles.js loaded - callback');
+});
